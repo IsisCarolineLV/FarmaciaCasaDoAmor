a8f15ca6e80a51a88947b1d2bdd79af6f44ccfaf
ItaloSLeao <italoleao200656@gmail.com>
2025-11-30 21:51:38 -0300

    adiciona inicialização do bd

--- dao/ConnectionFactory.java
+++ dao/ConnectionFactory.java
@@ -1,36 +1,20 @@
 package dao;
 
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class ConnectionFactory {
 
+    private static final String URL = "jdbc:mysql://localhost:3306/Farmacia?createDatabaseIfNotExist=true&useTimezone=true&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASS = "1234";
+
     public Connection getConnection() {
-        Properties props = carregarPropriedades();
-        String url = props.getProperty("db.url");
-        String user = props.getProperty("db.user");
-        String pass = props.getProperty("db.password");
-
         try {
-            return DriverManager.getConnection(url, user, pass);
+            return DriverManager.getConnection(URL, USER, PASS);
         } catch (SQLException e) {
-            throw new RuntimeException("Erro ao conectar no banco de dados: " + e.getMessage());
+            throw new RuntimeException("Erro ao conectar. Verifique se seu SGBD está rodando e se a senha é '1234'.", e);
         }
-    }
-
-    private Properties carregarPropriedades() {
-        Properties props = new Properties();
-        try (FileInputStream fs = new FileInputStream("db.properties")) {
-            props.load(fs);
-        } catch (IOException e) {
-            System.err.println("ERRO: O arquivo 'db.properties' não foi encontrado na raiz do projeto.");
-            System.err.println("Crie o arquivo baseando-se no 'db.properties.example'.");
-            throw new RuntimeException(e);
-        }
-        return props;
     }
 }
