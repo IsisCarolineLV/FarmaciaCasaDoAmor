569f5d3f865da3c3e738fa79251fffbcc6b8fc42
ItaloSLeao <italoleao200656@gmail.com>
2025-12-02 21:31:10 -0300

    muda o user do bd

--- dao/ConnectionFactory.java
+++ dao/ConnectionFactory.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class ConnectionFactory {
 
     private static final String URL = "jdbc:mysql://localhost:3306/Farmacia?createDatabaseIfNotExist=true&useTimezone=true&serverTimezone=UTC";
-    private static final String USER = "root";
+    private static final String USER = "admin";
     private static final String PASS = "1234";
 
     public Connection getConnection() {
