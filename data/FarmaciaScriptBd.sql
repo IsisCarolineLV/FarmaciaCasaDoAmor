3bafee24f1aa86e39d5fceb74a9a1888676408f2
isisCarolineLima <isiscarolin3@gmail.com>
2025-12-05 12:01:11 -0300

    bd padrao atualizado

--- data/FarmaciaScriptBd.sql
+++ data/FarmaciaScriptBd.sql
@@ -13,18 +13,18 @@ CREATE TABLE Estoque (
     IDEstoque INT PRIMARY KEY
 );
 
--- Insere o estoque 1
-INSERT INTO Estoque (IDEstoque) VALUES (1);
+INSERT INTO Estoque (IDEstoque) VALUES (1)
+ON CONFLICT (IDEstoque) DO NOTHING;
 
 CREATE TABLE Medicamento (
-    IDRemedio INT PRIMARY KEY, -- O codigo de barras
+    IDRemedio INT PRIMARY KEY,
+	Composicao VARCHAR(100),
     Nome VARCHAR(100) NOT NULL,
-    QuantidadeRemedio INT,
-    ValidadeRemedio DATE  
+    QuantidadeRemedio INT
 );
 
 CREATE TABLE Lote (
-    IDLote SERIAL PRIMARY KEY, -- SERIAL substitui AUTO_INCREMENT
+    IDLote SERIAL PRIMARY KEY,
     Validade DATE NOT NULL,
     QuantidadeComprimidos INT NOT NULL,
     IDRemedio INT NOT NULL,
@@ -33,11 +33,13 @@ CREATE TABLE Lote (
     FOREIGN KEY (IDEstoque) REFERENCES Estoque(IDEstoque)
 );
 
-CREATE TABLE Acessa (
-    IDEstoque INT,
-    CPF_Funcionario VARCHAR(14),
-    DataAcesso TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- TIMESTAMP substitui DATETIME
-    PRIMARY KEY (IDEstoque, CPF_Funcionario),
-    FOREIGN KEY (IDEstoque) REFERENCES Estoque(IDEstoque),
+CREATE TABLE Historico (
+    IDHistorico SERIAL PRIMARY KEY,
+    CPF_Funcionario VARCHAR(14) NOT NULL,
+    DataDia DATE DEFAULT CURRENT_DATE,
+    DataHora TIME DEFAULT CURRENT_TIME,
+    Acao VARCHAR(200) NOT NULL,
     FOREIGN KEY (CPF_Funcionario) REFERENCES Funcionario(CPF)
 );
+
+INSERT INTO funcionario (Nome, CPF) VALUES ('ADMIN', '123456789');
